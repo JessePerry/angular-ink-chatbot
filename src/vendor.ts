ff06c531dbf04564df525b8770b0e80dc71be402
JP <1jesseperry@gmail.com>
2019-08-31 11:22:47 +1000

    Adding more image assets

--- src/vendor.ts
+++ src/vendor.ts
@@ -4,4 +4,14 @@ import './assets/images/favicon-16x16.png';
 import './assets/images/favicon-32x32.png';
 import './assets/images/favicon-96x96.png';
 import './assets/images/weddinggame-600.png';
+import './assets/images/Binoculars.png';
+import './assets/images/Biota.png';
+import './assets/images/BiotaEntry.png';
+import './assets/images/Cricket.png';
+import './assets/images/Diet.png';
+import './assets/images/Flower1.png';
+import './assets/images/Flower2.png';
+import './assets/images/Mountain.png';
+import './assets/images/Space.png';
+import './assets/images/Train.png';
 
